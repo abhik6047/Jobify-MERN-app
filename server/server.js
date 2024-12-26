9f121807d8e178efeace4180989352574368884c
Abhijeet Karlewar <abhijeet.dipak@netcomlearning.com>
2024-12-26 14:40:02 +0530

    Added console logs for debugging on vercel

--- server/server.js
+++ server/server.js
@@ -1,25 +1,24 @@
-import "express-async-errors";
-import * as dotenv from "dotenv";
-dotenv.config();
-import express from "express";
-import morgan from "morgan";
-import mongoose from "mongoose";
-import cookieParser from "cookie-parser";
 import cloudinary from "cloudinary";
+import cookieParser from "cookie-parser";
+import * as dotenv from "dotenv";
+import express from "express";
+import "express-async-errors";
+import mongoose from "mongoose";
+import morgan from "morgan";
+dotenv.config();
 
 // Custom imports
-import jobRouter from "./routes/jobRouter.js";
 import authRouter from "./routes/authRouter.js";
+import jobRouter from "./routes/jobRouter.js";
 import userRouter from "./routes/userRouter.js";
 
 // public
-import { dirname } from "path";
+import path, { dirname } from "path";
 import { fileURLToPath } from "url";
-import path from "path";
 
 // Middleware imports
-import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import { authenticateUser } from "./middleware/authMiddleware.js";
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 
 cloudinary.config({
 	cloud_name: process.env.CLOUD_NAME,
@@ -45,6 +44,13 @@ app.use(express.json());
 app.use("/api/v1/jobs", authenticateUser, jobRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 app.use("/api/v1/auth", authRouter);
+
+app.get("/", (req, res) => {
+	res.json({
+		message: "API is working",
+		success: true,
+	});
+});
 
 app.get("*", (req, res) => {
 	res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
