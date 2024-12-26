96c15c4663bce977a42813cc13ed1716403b43d8
Abhijeet Karlewar <abhijeet.dipak@netcomlearning.com>
2024-12-26 18:18:04 +0530

    Added origin in server.js

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(
 	cors({
-		origin: "http://localhost:5173",
+		origin: "https://jobify-mern-app-frontend.vercel.app",
 		credentials: true,
 	})
 );
